47e29d62cc2aad5f066da13a6f2692c1b8b7645a
joao-ah <joao.costalonga@escola.pr.gov.br>
2025-10-06 11:02:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -78,5 +78,14 @@ alturaTd.textContent = altura;
 gorduraTd.textContent = gordura;
 imcTd.textContent = imc;
 
+//adiciona as tags na tela o usuario 
+pacienteTr.appendChild(nomeTd);
+pacienteTr.appendChild(pesoTd);
+pacienteTr.appendChild(alturaTd);
+pacienteTr.appendChild(gorduraTd);
+pacienteTr.appendChild(imcTd);
+
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.appendChild(pacienteTr);
 });
 
